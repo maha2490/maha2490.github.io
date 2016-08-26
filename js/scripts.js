d4aec46a83318b9b1afc6855c2a93583c1a5aba3
Rachel Mahan <rachelm2490@gmail.com>
2016-08-26 16:14:53 -0400

    static contact form test

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ function submitForm(){
  
     $.ajax({
       type: "POST",
-      url: "php/form-process.php",
+      url: "php/process.php",
       data: "name=" + name + "&email=" + email + "&message=" + message,
       success : function(text){
           if (text == "success"){
