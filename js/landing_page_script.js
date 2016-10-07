0cbce76aaf25ca3198a35d140a7d60505702f435
Rachel Mahan <rachelm2490@gmail.com>
2016-10-07 15:55:05 -0400

    animation timing adjustmnets

--- js/landing_page_script.js
+++ js/landing_page_script.js
@@ -11,23 +11,16 @@ $(function(){
 
 //fade in on scroll down
 $(function() {
-    $(window).scroll( function(){
-    
-       
+    $(window).scroll( function(){      
         $('.fade_in_block').each( function(i){
             
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* Adjust the "200" to either have a delay or that the content starts fading a bit before you reach it  */
-            bottom_of_window = bottom_of_window + 200;  
-          
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
+            bottom_of_window = bottom_of_window + 400;  
+
+            if( bottom_of_window > bottom_of_object ){                
+                $(this).animate({'opacity':'1'},1000);                   
             }
-        }); 
-    
+        });    
     });
 });
